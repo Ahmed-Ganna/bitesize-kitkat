781480b150ebdf166b1b4acc92c7a08e430c567a
Sam Davies <sdavies@shinobicontrols.com>
2014-01-15 15:28:53 +0000

    Can now set MessageMe as the default messaging app
    Had to add a distinct broadcast receiver for MMS

---
+++ MessageMe/messageme/src/main/java/com/shinobicontrols/messageme/receivers/MMSBroadcastReceiver.java
@@ -0,0 +1,17 @@
+package com.shinobicontrols.messageme.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MMSBroadcastReceiver extends BroadcastReceiver {
+    public MMSBroadcastReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
